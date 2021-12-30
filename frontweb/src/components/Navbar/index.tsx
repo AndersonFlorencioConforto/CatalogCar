3470dc4b066fa7dc87161a835a81350c344538ce
Anderson Conforto <afcaconforto@gmail.com>
2021-12-30 20:57:40 -0300

    first commit

---
+++ frontweb/src/components/Navbar/index.tsx
@@ -0,0 +1,44 @@
+import './styles.css';
+import 'bootstrap/js/src/collapse.js';
+import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar navbar-expand-md navbar-dark bg-primary main-nav">
+      <div className="container-fluid">
+        <Link to="/" className="nav-logo-text">
+          <h4>Carros Top</h4>
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#topcar-navbar"
+          aria-controls="topcar-navbar"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="topcar-navbar">
+          <ul className="navbar-nav offset-md-2 main-menu">
+            <li>
+              <NavLink to="/" activeClassName="active" exact>
+                HOME
+              </NavLink>
+            </li>
+            <li>
+              <NavLink to="/products" activeClassName="active">
+                CATÁLOGO
+              </NavLink>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
